97ed2bcd7e36a4533d2ea252b529272fae776e73
Zac Allen <zac.allen@outlook.com>
2020-06-03 12:37:20 +0100

    added search function, added cart link to navbar

--- shutter/settings.py
+++ shutter/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
+    'search',
 
 ]
 
@@ -131,11 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
