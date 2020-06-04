d9e73254e3d534eba03200bb503fcb427152f1b7
Zac Allen <zac.allen@outlook.com>
2020-06-04 15:24:08 +0100

    checkout app added, stripe used as payment method, working.

--- shutter/settings.py
+++ shutter/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'search',
+    'checkout',
 
 ]
 
@@ -139,3 +140,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
+STRIPE_SECRET = os.getenv("STRIPE_SECRET")
