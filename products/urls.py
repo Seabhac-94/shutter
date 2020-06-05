0cf15c3a36907f97bfc7f41b1347751da4eb5b52
Zac Allen <zac.allen@outlook.com>
2020-06-05 21:03:35 +0100

    continued styling index, links to individual collections set up

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from .views import all_products, irishness_products, nature_products, still_life
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
-    url(r'^still_life', still_life_products, name='still_life_products'),
-    url(r'^adventure', adventure_products, name='adventureproducts'),
-    url(r'^nature', nature_products, name='natureproducts'),
-    url(r'^irishness', irishness_products, name='irishproducts')
+    url(r'^still_life', still_life_products, name='still_life'),
+    url(r'^adventure', adventure_products, name='adventure'),
+    url(r'^nature', nature_products, name='nature'),
+    url(r'^irishness', irishness_products, name='irish')
 ]
